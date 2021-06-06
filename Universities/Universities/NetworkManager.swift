51ed0746331531a1910e0bd209723b1db09679ed
BlazJurisic <jurisic.blaz@gmail.com>
2021-06-06 19:30:14 +0200

    initial commit

---
+++ Universities/Universities/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Universities
+//
+//  Created by Student on 11.05.2021..
+//  Copyright © 2021 RMA. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager: NSObject {
+
+}
---
+++ Universities/Universities/University.swift
@@ -0,0 +1,13 @@
+//
+//  University.swift
+//  Universities
+//
+//  Created by Student on 11.05.2021..
+//  Copyright © 2021 RMA. All rights reserved.
+//
+
+import UIKit
+
+class University: NSObject {
+
+}
